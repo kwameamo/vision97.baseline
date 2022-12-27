86f44aae94b71728776a97eadd43615e1840c268
kwameamo <118262204+kwameamo@users.noreply.github.com>
2022-12-27 14:52:24 +0000

    switched up the whole flow

---
+++ scripts/desk-Cart.js
@@ -0,0 +1,14 @@
+/* Open cart */
+  function openDeskCart() {
+    document.getElementById("deskCartSlide").style.width = "30%";
+  }
+
+  /* Close cart */
+  function closeDeskCart() {
+    document.getElementById("deskCartSlide").style.width = "0%";  
+  } 
+  
+
+
+
+
--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -1,20 +1,27 @@
-/* Open nav */
-function openNav() {
-    document.getElementById("mobile-navigation").style.width = "90%";
+/* Mobile Menus*/ 
+
+  /* Open nav */
+  function openNav() {
+      document.getElementById("mobile-navigation").style.width = "90%";
+    }
+
+    /* Close nav */
+  function closeNav() {
+      document.getElementById("mobile-navigation").style.width = "0%"; 
+    }
+
+
+  /* Open cart */
+  function openCart() {
+    document.getElementById("cartSlide").style.width = "90%";
   }
 
-  /* Close nav */
-function closeNav() {
-    document.getElementById("mobile-navigation").style.width = "0%"; 
-  }
+  /* Close cart */
+  function closeCart() {
+    document.getElementById("cartSlide").style.width = "0%";  
+  } 
+  
 
 
-/* Open cart */
-function openCart() {
-  document.getElementById("cartSlide").style.width = "90%";
-}
 
-/* Close cart */
-function closeCart() {
-  document.getElementById("cartSlide").style.width = "0%";  
-}
+
